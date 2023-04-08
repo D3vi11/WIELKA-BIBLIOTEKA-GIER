146976c48d1cdee9be42e2b91fb27f3c7dec411f
D3vi11 <krystianb69@gmail.com>
2023-04-09 00:28:06 +0200

    changing almost everything to constraint layout

--- app/src/main/java/com/example/greatgamelibrary/MainActivity.kt
+++ app/src/main/java/com/example/greatgamelibrary/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.greatgamelibrary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var editText: EditText = findViewById(R.id.editText)
+        var searchButton: Button = findViewById(R.id.searchButton)
+        searchButton.setOnClickListener {
+            Toast.makeText(this,editText.text,Toast.LENGTH_SHORT).show()
+        }
     }
 }
