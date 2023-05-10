6d49f11c48af2c91dff9629aee12fb36684834aa
D3vi11 <krystianb69@gmail.com>
2023-05-10 12:30:45 +0200

    Start

--- app/src/main/java/com/example/greatgamelibrary/activities/LoginActivity.kt
+++ app/src/main/java/com/example/greatgamelibrary/activities/LoginActivity.kt
@@ -1,28 +1,36 @@
 package com.example.greatgamelibrary.activities
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.greatgamelibrary.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var loginText: EditText
-    lateinit var passwordText: EditText
+    lateinit var loginText: TextView
+    lateinit var passwordText: TextView
     lateinit var loginButton: Button
     lateinit var registerButton: Button
     lateinit var mainMenuButton: Button
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = Firebase.auth
         loginText = findViewById(R.id.Login)
         passwordText = findViewById(R.id.Password)
         loginButton = findViewById(R.id.LoginButton)
         registerButton = findViewById(R.id.RegisterButton)
         mainMenuButton = findViewById(R.id.MainMenuButton)
         loginButton.setOnClickListener {
-            login()
+            login(loginText.text.toString(),passwordText.text.toString())
         }
         registerButton.setOnClickListener {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
@@ -36,7 +44,27 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    fun login(){
-
+    fun login(email:String,password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    val intent = Intent(this@LoginActivity, GameActivity::class.java)
+                    intent.putExtra("isLoggedIn",true)
+                    startActivity(intent)
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    //updateUI(null)
+                }
+            }
     }
 }
