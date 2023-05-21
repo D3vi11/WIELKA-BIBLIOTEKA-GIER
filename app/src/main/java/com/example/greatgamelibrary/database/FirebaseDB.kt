68c11775024e1eddef5a09b665011e285d1a6e6e
D3vi11 <krystianb69@gmail.com>
2023-05-21 22:28:47 +0200

    audio is done

--- app/src/main/java/com/example/greatgamelibrary/database/FirebaseDB.kt
+++ app/src/main/java/com/example/greatgamelibrary/database/FirebaseDB.kt
@@ -30,7 +30,7 @@ class FirebaseDB(var activity: ActivityInterface) {
                 gameImage = initializeArray(snapshot.childrenCount.toInt())
                 for (i in 0 until snapshot.childrenCount) {
                     gameDataList.add(GameInfo(snapshot.child(i.toString()).child("game")))
-                    getDataFromStorage(gameDataList[i.toInt()].imageName, i.toInt())
+                    getDataFromStorage(gameDataList[i.toInt()].imageName,gameDataList[i.toInt()].audioName,gameDataList[i.toInt()].videoName, i.toInt())
                 }
             }
 
@@ -41,10 +41,10 @@ class FirebaseDB(var activity: ActivityInterface) {
         return gameDataList
     }
 
-    fun getDataFromStorage(imageName: String, item: Int) {
+    fun getDataFromStorage(imageName: String, audioName: String, videoName: String, item: Int) {
         var imageRef = storageReference.child("image/${imageName}")
-        var audioRef = storageReference.child("audio/${imageName}")
-        var videoRef = storageReference.child("video/${imageName}")
+        var audioRef = storageReference.child("audio/${audioName}")
+        var videoRef = storageReference.child("video/${videoName}")
         val ONE_MEGABYTE: Long = 1024 * 1024
         titleRef.getBytes(ONE_MEGABYTE).addOnSuccessListener {
             gameImage.add(index,GameImage(BitmapFactory.decodeByteArray(it, 0, it.size)))
@@ -59,7 +59,16 @@ class FirebaseDB(var activity: ActivityInterface) {
         }.addOnFailureListener {
             it.stackTrace
         }
-        gameAudio.add(GameAudio(audioRef.downloadUrl.result))
+        var uri = audioRef.downloadUrl
+
+        uri.addOnSuccessListener {
+            gameAudio.add(GameAudio(uri.result))
+            activity.onUpdate()
+        }.addOnFailureListener{
+            it.stackTrace
+        }
+
+
     }
 
     fun initializeArray(size: Int): ArrayList<GameImage>{
